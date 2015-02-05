f690c41f4b9061665ed5b3a127862f14414c05e6
Josh <josh.rickers@googlemail.com>
2015-02-05 23:37:10 +0000

    More film info + user of callback
    Added film ratings, genre and plot. Proper use of callback.

--- public/js/main.js
+++ public/js/main.js
@@ -11,29 +11,35 @@
 		return url;
 	}
 
-	function getFilmInfo(url){
-		var filmData;
+	function getFilmInfo(url, callback){
 		$.get(url, function(data){
-			showPopover(JSON.parse(data), target);
+			//showPopover(JSON.parse(data), target);
+			callback(JSON.parse(data), target);
 		});
 	}
 
 	function showPopover(filmData, target){
-		target.popover({ 
-			trigger: 'hover', 
+		target.popover({
+			trigger: 'manual',
 			placement: 'right',
-			title: filmData.Title 
+			html: true,
+			title: filmData.Title,
+			content: '<h5>Rating: '+ filmData.imdbRating +'</h5><h5>Genre: ' + filmData.Genre + '</h5><p>' + filmData.Plot + '</p>'
 		}).popover('show');
 	}
 
 	$(document).ready(function() {
-		$(document).on('mouseover', 'a', function(e){
+		$(document).on('mouseenter', 'a', function(e){
 			target = $(e.currentTarget);
 			href = $(this).attr('href');
 			while((urls = regex.exec(href)) !== null) {
 				omdburl = createOMDB(urls[1]);
-				getFilmInfo(omdburl);
+				getFilmInfo(omdburl, function(filmData, target){
+					showPopover(filmData, target);
+				});
 			}
+		}).on('mouseleave', 'a', function(){
+			target.popover('destroy');
 		});
 	});
 })(window);
